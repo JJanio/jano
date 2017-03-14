e0cfbab9464860caf1513887a12c7bd4aa8d4a99
jjanio <facesdejano@gmail.com>
2017-03-14 12:42:09 -0300

    Começo da criação de um blog

---
+++ www/facesdejano/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> FACES DE JANO </title>
+</head>
+<body>
+
+<p>Quando precisamos mudar o momento sempre é o agora...</p>
+
+</body>
+</html>
